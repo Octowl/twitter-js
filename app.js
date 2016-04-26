f919f2bc7a4f7e6a49737c3f5f6cf821a502b578
COREY <coreygreenwald@Coreys-MacBook-Pro.local>
2016-04-26 16:31:13 -0400

    updates under sir corey

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var chalk = require('chalk');
 var morgan = require('morgan');
 var express = require('express');
+var swig = require('swig');
+swig.setDefaults({ cache: false });
+
+
 var app = express();
+//var swigResult = swig.compileFile('views/index.html');
+app.engine('html',swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.use(morgan('tiny'))
 
 app.use('/special', function(req, res, next){
-    console.log(chalk.magenta('You has reached the special area! You are a genius!'))
+    console.log(chalk.magenta('You has reached the special area! You are a genius!'));
     next();
 })
 
 app.get('/', function(req, res){
-    res.send("Welcome!");
+	res.render('index', {title: "TaiLer swift's Home Page", people: [{name: 'Find out more!'}, {name: "she's awesome!"}]});
 })
 
 app.get('/news', function(req,res){
